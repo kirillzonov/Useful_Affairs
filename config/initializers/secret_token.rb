fa6fa06d8350ea210d6d98469168f484fb1eaa38
Kirill Zonov <graffzon@gmail.com>
2010-12-09 21:09:14 +0300

    initial commit and plugin restful_auth install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UsefulAffairs::Application.config.secret_token = 'f5987e02a0c855e3cdb624ea496063ee245d6584d5d6c33ac83856702f8de0349daddde5ce98a60f4b3ba346a93c5df381f562c0ef8e901dd8a21eb66933677f'
